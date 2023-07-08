c5037d7138b159be662837301919837462f54bcb
kmstorm <khaminhbao17082003@gmail.com>
2023-07-09 06:56:42 +0700

    update

--- UsedDataset/InsertTo.py
+++ UsedDataset/InsertTo.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
-
-data = pd.read_csv("Director.csv")
+data = pd.read_csv("UsedDataset/Movie.csv")
 dataDict = data.to_dict()
-print(dataDict)
-with open("Director.txt", "w") as file:
+# print(dataDict)
+with open("UsedDataset/Movie.txt", "w") as file:
     for i in range(710):
         MovieID = dataDict["MovieID"][i]
-        Director = dataDict["Director"][i]
-        string = f"insert into dbo.Director values ('{Director}','{MovieID}')"
+        Title = dataDict["Title"][i]
+        url = dataDict["URL"][i]
+        Type = dataDict["Title Type"][i]
+        Rate = dataDict["IMDb Rating"][i]
+        Time = dataDict["Runtime (mins)"][i]
+        Year = dataDict["Year"][i]
+        Genres = dataDict["Genres"][i]
+        Poster = dataDict["Poster"][i]
+        string = f"insert into dbo.Movie values('{MovieID}','{Title}','{url}','{Type}','{Rate}','{Time}','{Year}','{Genres}','{Poster}')"
         file.write(string+"\n")
  
